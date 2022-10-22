e2383c802b276162bf511a6e9a8c23020f72e9e2
ismailabay <ismailabay.dev@gmail.com>
2022-10-22 20:21:42 +0300

    learn c#

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("1.sınav notu gir: ");
+int sınav1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2.sınav notu gir:");
+int sınav2 = Convert.ToInt32(Console.ReadLine());
+
+decimal notOrtalaması = Convert.ToDecimal (sınav1+sınav2) / 2;
+Console.WriteLine("not ortalaması: " +notOrtalaması);
+Console.ReadKey();
